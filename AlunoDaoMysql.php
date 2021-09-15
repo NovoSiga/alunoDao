87a36b79b7be6372667837b2da96b4981fc31ad4
Pedro H Lopes <85568654+pedrohlopesnvp@users.noreply.github.com>
2021-09-15 19:18:17 -0300

    Update AlunoDaoMysql.php

--- AlunoDaoMysql.php
+++ AlunoDaoMysql.php
@@ -1,35 +1,89 @@
 <?php
 
-class Aluno {
-    private $id;
-    private $nome;
-    private $turma;
-    private $curso;
+require_once 'C:/xampp/htdocs/NovoSiga/models/Aluno.php';
+
+class AlunoDaoMysql {
+
+    private $pdo;
+
+    public function __construct(PDO $engine){
+        $this->pdo = $engine;
+    }
+  
+    public function alunoGenerator ($array){
+        $u = new Aluno ();
+        $u->setId($array['id'])?? 0;
+        $u->setNome($array['nome'])?? 0;
+        $u->setTurma($array['turma'])?? 0;
+        $u->setCurso($array['curso'])?? 0;
+
+        return $u;
+
+    }
+    public function findAll()
+    {
+        $array = [];
+
+        $sql = $this->pdo->query("SELECT * FROM aluno");
+        if ($sql->rowCount()>0){
+            $dados = $sql->fetchAll();
+
+            foreach($dados as $item){
+           
+                $aluno = $this->alunoGenerator($item);
+                $array[] = $aluno;
+            }
+        }
+        return $array;
+    }
+    public function findById($id)
+    {
+        $sql = $this->pdo->prepare("SELECT * FROM aluno WHERE id=:id");
+        $sql->bindValue(':id', $id);
+        $sql->execute();
+
+        if($sql->rowCount()>0){
+            $info = $sql->fetch(PDO::FETCH_ASSOC);
+            $user=$this->alunoGenerator($info);
+            return $user;
+        }else{
+            return false;
+        }
+    }
     
+    public function addAluno(Aluno $m)
+    {
+        $sql = $this->pdo->prepare("INSERT INTO aluno (nome, turma, curso) VALUES (:nome, :turma, :curso)");
+        $sql->bindValue(':nome',$m->getNome());
+        $sql->bindValue(':turma', $m->getTurma());
+        $sql->bindValue(':curso', $m->getCurso());
+        $sql->execute();
 
-    public function getId (){
-        return $this->id;
+        $m->setId($this->pdo->lastInsertId());
+        return true;
     }
-    public function setId ($i){
-        $this->id = $i;
+
+    public function deleteAlun($id)
+    {
+        $sql=$this->pdo->prepare("DELETE FROM aluno WHERE id=:id");
+        $sql->bindValue(':id', $id);
+        $sql->execute();
     }
-    public function getNome (){
-        return $this->nome;
+
+    public function updateAluno (Aluno $u){
+        $sql = $this->pdo->prepare("UPDATE aluno SET nome=:nome, turma=:turma, curso=:curso WHERE id=:id");
+        $sql->bindValue(':nome',$u->getNome() );
+        $sql->bindValue(':turma', $u->getTurma());
+        $sql->bindValue(':curso', $u->getCurso());
+        $sql->bindValue(':id', $u->getId());
+        $sql->execute();
+
+        return true;
     }
-    public function setNome ($i){
-        $this->nome = $i;
+    public function deleteAluno($id){
+        $sql=$this->pdo->prepare("DELETE FROM aluno WHERE id=:id");
+        $sql->bindValue(':id', $id);
+        $sql->execute();
     }
-    public function getTurma (){
-        return $this->turma;
-    }
-    public function setTurma ($i){
-        $this->turma = $i;
-    }
-    public function getCurso (){
-        return $this->curso;
-    }
-    public function setCurso ($i){
-        $this->curso = $i;
-    }
+
 }
-
